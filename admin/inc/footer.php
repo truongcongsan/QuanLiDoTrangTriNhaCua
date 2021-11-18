df0116b8d3134efb2260ba492641d401053d4f85
Truong <firelcub95@gmail.com>
2021-11-18 12:10:25 +0700

    upload

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+ <div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         Trịnh Nam Trường - Tin12a1 - 18103100003
+        </p>
+    </div>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,57 @@
+<?php 
+	include 'inc/header.php';
+ ?>
+
+ <div class="main">
+    <div class="content">
+    	<div class="support">
+  			<div class="support_desc">
+  				<h3>Hỗ trợ trực tuyến</h3>
+  				<p><span>24 giờ | 7 ngày trong tuần | 365 ngày một năm &nbsp;&nbsp; Hỗ trợ kỹ thuật trực tiếp</span></p>
+  			</div>
+  				<img src="web/images/contact.png" alt="" />
+  			<div class="clear"></div>
+  		</div>
+    	<div class="section group">
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Liên hệ chúng tôi</h2>
+					    <form>
+					    	<div>
+						    	<span><label>Họ tên</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-mail</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>Điện thoại</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>Nội dung</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Xác nhận"></span>
+						  </div>
+					    </form>
+				  </div>
+  				</div>
+				<div class="col span_1_of_3">
+      			<div class="company_address">
+				     	<h2>Thông tin công ty :</h2>
+				   		<p>Điện thoại:(00) 222 666 444</p>
+				   		<p>Fax: (000) 000 00 00 0</p>
+				 	 	<p>Email: <span>info@mycompany.com</span></p>
+				   		<p>Theo dõi trên: <span>Facebook</span>, <span>Twitter</span></p>
+				   </div>
+				 </div>
+			  </div>    	
+    </div>
+ </div>
+
+<?php 
+	include 'inc/footer.php';
+ ?>
